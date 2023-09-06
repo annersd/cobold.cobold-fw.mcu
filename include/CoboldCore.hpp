e7dfb84c57189988306f86b854f77c2a2491f072
Qargh <qargh@online.ms>
2023-09-06 23:01:47 +0200

    move to include

---
+++ include/CoboldCore.hpp
@@ -0,0 +1,24 @@
+#pragma once
+
+#define COBOLD_CORE_VERSION_MAJOR 0
+#define COBOLD_CORE_VERSION_MINOR 1
+#define COBOLD_CORE_VERSION_PATCH 0
+
+#include "Logger.h"
+#include "IApplication.h"
+#include "ServiceCollection.h"
+#include "ServiceCollectionExtensions.h"
+
+// Include Hosting
+#include "IHost.h"
+#include "IHostBuilder.h"
+#include "IHostedService.h"
+#include "Host.h"
+#include "HostBuilder.h"
+#include "HostingExtensions.h"
+
+// Include Configuration
+#include "IConfiguration.h"
+#include "Configuration.h"
+
+#include "Application.h"
--- lib/cobold.core/src/CoboldCore.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#define COBOLD_CORE_VERSION_MAJOR 0
-#define COBOLD_CORE_VERSION_MINOR 1
-#define COBOLD_CORE_VERSION_PATCH 0
-
-#include "Logger.h"

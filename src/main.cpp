bb4ff8a9cd62d23598522955ffab404570a783d1
Qargh <qargh@online.ms>
2023-09-10 11:13:14 +0200

    adding secret settings

--- src/main.cpp
+++ src/main.cpp
@@ -2,12 +2,20 @@
 #include "Cobold.hpp"
 #include "CoboldHosting.hpp"
 #include "Scheduler.h"
+#include "secrets.h"
 
 void setup()
 {
   // put your setup code here, to run once:
 
+Serial.begin(115200);
   cobold::app = new cobold::Application(new cobold::hosting::HostBuilder());
+
+  cobold::app->getAppConfiguration()->setValue("cobold.network.wifi.ssid", "");
+  cobold::app->getAppConfiguration()->setValue("cobold.network.wifi.password", "");
+  
+  configureApplicationSecrets();
+
   cobold::app->preSetup();
   cobold::app->setup();
 
@@ -17,12 +25,14 @@ void setup()
                              { AsyncJsonResponse *response = new AsyncJsonResponse(false, 4096); });
   // (Optional) Add sketch size and free PSRAM (if available)
   cobold::app->run();
+
 }
 
 void loop()
 {
   // put your main code here, to run repeatedly:
   cobold::app->loop();
+
 }
 
 void setupExamples()
@@ -33,9 +43,4 @@ void setupExamples()
       1000, [](const Scheduler::StateObject &state) -> void
       { Serial.println("Hello World"); },
       "HelloWorld", 1000, Scheduler::StateObject());
-
-  cobold::app->getHostBuilder()->configureAppConfiguration([](cobold::configuration::IConfiguration *configuration) -> void
-                                                           {
-    configuration->setValue("cobold.network.wifi.ssid", "MySSID");
-    configuration->setValue("cobold.network.wifi.password", "MyPassword"); });
 }

7940bd66fa70ccd2752abc91e5f7c696a7cdd4a2
Qargh <qargh@online.ms>
2023-09-13 16:37:32 +0200

    network optimisations and auto reconnect

--- lib/cobold.core/src/Network.cpp
+++ lib/cobold.core/src/Network.cpp
@@ -1,12 +1,20 @@
 #include "Network.h"
 
-
 // #include "ServiceCollection.h"
 #include "IApplication.h"
 #include "Logger.h"
 
 Network::Network(String ssid, String password)
-    {
-        this->ssid = ssid;
-        this->password = password;
-    }
+{
+    this->ssid = ssid;
+    this->password = password;
+
+    wifiReconnectTimer = xTimerCreate("wifiTimer", pdMS_TO_TICKS(2000), pdFALSE, (void *)0, [](TimerHandle_t xTimer) -> void
+                                      {
+                                          Network *self = static_cast<Network *>(pvTimerGetTimerID(xTimer));
+                                          if (self != nullptr)
+                                          {
+                                              self->connectToWifi();
+                                          }
+                                      });
+}
--- lib/cobold.core/src/Network.h
+++ lib/cobold.core/src/Network.h
@@ -14,48 +14,59 @@
 #include "AsyncJson.h"
 #include "ArduinoJson.h"
 
+/**
+ * @brief Class for managing the network connection.
+ * 
+ * This class is responsible for connecting to the IP network.
+ * 
+ * @todo Add support for Ethernet
+ * @todo Handle WiFi connection errors carefully, especially how often to try to reconnect
+*/
 class Network
 {
 private:
     String ssid;
     String password;
     cobold::Logger *logger;
+    TimerHandle_t wifiReconnectTimer;
 
-    static void WiFiEvent(WiFiEvent_t event)
+    void connectToWifi()
     {
-        Serial.printf("[WiFi-event] event: %d\n", event);
+        Serial.println("Connecting to Wi-Fi...");
+        WiFi.begin(ssid, password);
+    }
+
+    void WiFiEvent(WiFiEvent_t event, WiFiEventInfo_t info)
+    {
         switch (event)
         {
         case SYSTEM_EVENT_WIFI_READY:
-            Serial.println("WiFi interface ready");
+            logger->info("[WiFi-event] WiFi interface ready");
             break;
         case SYSTEM_EVENT_SCAN_DONE:
-            Serial.println("Completed scan for access points");
+            logger->info("[WiFi-event] Completed scan for access points");
             break;
         case SYSTEM_EVENT_STA_START:
-            Serial.println("WiFi client started");
+            logger->info("[WiFi-event] WiFi client started");
             break;
         case SYSTEM_EVENT_STA_STOP:
-            Serial.println("WiFi clients stopped");
+            logger->info("[WiFi-event] WiFi clients stopped");
+            // start the timer to try to reconnect to WiFi every in 2 seconds
+            xTimerStart(wifiReconnectTimer, 0);
             break;
         case SYSTEM_EVENT_STA_CONNECTED:
-            Serial.println("Connected to access point");
+            logger->info("[WiFi-event] Connected to access point");
             break;
-
         case SYSTEM_EVENT_STA_AUTHMODE_CHANGE:
-            Serial.println("Authentication mode of access point has changed");
+            logger->info("[WiFi-event] Authentication mode of access point has changed");
             break;
-
         case SYSTEM_EVENT_STA_GOT_IP:
-            Serial.println("WiFi connected");
-            Serial.println("IP address: ");
-            Serial.println(WiFi.localIP());
-            // connectToMqtt();
+            logger->info("[WiFi-event] WiFi connected; IP address: %s", WiFi.localIP().toString());
             break;
         case SYSTEM_EVENT_STA_DISCONNECTED:
-            Serial.println("WiFi lost connection");
-            // xTimerStop(mqttReconnectTimer, 0); // ensure we don't reconnect to MQTT while reconnecting to Wi-Fi
-            // xTimerStart(wifiReconnectTimer, 0);
+            logger->info("[WiFi-event] WiFi lost connection");
+            // start the timer to try to reconnect to WiFi every in 2 seconds
+            xTimerStart(wifiReconnectTimer, 0);
             break;
         }
     }
@@ -72,9 +83,10 @@ public:
         logger->debug("Connecting to %s", ssid);
         logger->debug("Password: %s", password);
 
-        WiFi.onEvent(WiFiEvent);
+        WiFi.onEvent([this](WiFiEvent_t event, WiFiEventInfo_t info)
+                     { this->WiFiEvent(event, info); });
 
-        WiFi.begin(ssid, password);
+        connectToWifi();
         if (WiFi.waitForConnectResult() != WL_CONNECTED)
         {
             logger->fatal("WiFi Failed!");

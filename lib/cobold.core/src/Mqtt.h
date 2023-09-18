79cbf3ec5e138eecaf63607eaccf31ab821d9e35
Qargh <qargh@online.ms>
2023-09-18 23:25:39 +0200

    wip mqtt service

--- lib/cobold.core/src/Mqtt.h
+++ lib/cobold.core/src/Mqtt.h
@@ -14,6 +14,14 @@ namespace cobold::services
     namespace mqtt
     {
         typedef std::function<void(MqttEventArgs *eventargs)> OnMessageCallback;
+
+        struct MessageCallback
+        {
+            std::string topic;
+            OnMessageCallback callback;
+        };
+
+        
     }; // namespace mqtt
 
     class Mqtt
@@ -21,7 +29,7 @@ namespace cobold::services
     private:
         cobold::configuration::IConfiguration *mqttConfig;
         cobold::IApplication *app;
-        std::vector<mqtt::OnMessageCallback> onMessageCallbacks;
+        std::vector<mqtt::MessageCallback> onMessageCallbacks;
         cobold::Logger *logger;
 
         std::string mqttPrefix = "cobold/";
@@ -40,23 +48,27 @@ namespace cobold::services
             Serial.println(mqttNodePrefix.c_str());
         }
 
-        void onMessage(mqtt::OnMessageCallback callback)
+        void onMessage(std::string topic, mqtt::OnMessageCallback callback)
         {
-            this->onMessageCallbacks.push_back(callback);
+            this->onMessageCallbacks.push_back(mqtt::MessageCallback{topic, callback});
         }
 
         void handleMqttMessage(char *topic, char *payload, AsyncMqttClientMessageProperties properties, size_t len, size_t index, size_t total)
         {   
+            logger->debug("Dispatching mqtt event: %s", topic);
+            for (auto callback : this->onMessageCallbacks)
+            {
+                if (std::regex_match(std::string(topic), std::regex(callback.topic)))
+                {
+                    callback.callback(new MqttEventArgs(new std::string(topic), payload));
+                }
+            }
+
             auto top = std::string(topic);
             top = std::regex_replace(top, std::regex(mqttNodePrefix), "$local/");
 
             auto eventArgs = new MqttEventArgs(new std::string(top.c_str()), payload);
           
-            // for (auto &callback : this->onMessageCallbacks)
-            // {
-            //     callback(eventArgs);
-            // }
-
             logger->debug("Dispatching local event: %s", top.c_str());
             app->raiseEvent(cobold::sys::Event::create("cobold.mqtt.message", "string", eventArgs));
         }

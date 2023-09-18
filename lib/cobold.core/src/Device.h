6a01cd951cbf2fce74e160da9aaa2d86db7fb432
Qargh <qargh@online.ms>
2023-09-18 23:23:52 +0200

    init Node and Device classes

---
+++ lib/cobold.core/src/Device.h
@@ -0,0 +1,16 @@
+#pragma once
+
+namespace cobold
+{
+    /**
+     * @brief A cobold device represents a physical device that is connected to the cobold network.
+    */
+    class Device
+    {
+    private:
+        /* data */
+    public:
+        Device(/* args */){};
+        ~Device() = default;
+    };
+}
---
+++ lib/cobold.core/src/Node.h
@@ -0,0 +1,16 @@
+#pragma once
+
+namespace cobold
+{
+    /**
+     * @brief A node represents a logical unit in the cobold network.
+    */
+    class Node
+    {
+    private:
+        /* data */  
+    public:
+        Node(/* args */){};
+        ~Node() = default;
+    };
+}
